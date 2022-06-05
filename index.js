30d49e4263405ee686c84363d7be97b06969e2a5
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-05 12:10:25 -0400

    added starter functions for each team member to the index.js file

--- index.js
+++ index.js
@@ -1,1 +1,104 @@
+function Manager(name, occupation, ID, email, officeNum) {
+  this.name = name;
+  this.occupation = occupation;
+  this.ID = ID;
+  this.email = email;
+  this.officeNum = officeNum;
+}
 
+function EmployeeOne(name, occupation, ID, email, github) {
+  this.name = name;
+  this.occupation = occupation;
+  this.ID = ID;
+  this.email = email;
+  this.github = github;
+}
+
+function EmployeeTwo(name, occupation, ID, email, github) {
+  this.name = name;
+  this.occupation = occupation;
+  this.ID = ID;
+  this.email = email;
+  this.github = github;
+}
+
+function EmployeeThree(name, occupation, ID, email, github) {
+  this.name = name;
+  this.occupation = occupation;
+  this.ID = ID;
+  this.email = email;
+  this.github = github;
+}
+
+function Intern(name, occupation, ID, email, school) {
+  this.name = name;
+  this.occupation = occupation;
+  this.ID = ID;
+  this.email = email;
+  this.school = school;
+}
+
+Manager.prototype.printInfo = function () {
+  console.log(
+    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nOffice Number: ${this.officeNum}`
+  );
+  console.log("\n-------------\n");
+};
+
+EmployeeOne.prototype.printInfo = function () {
+  console.log(
+    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
+  );
+  console.log("\n-------------\n");
+};
+
+EmployeeTwo.prototype.printInfo = function () {
+  console.log(
+    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
+  );
+  console.log("\n-------------\n");
+};
+
+EmployeeThree.prototype.printInfo = function () {
+  console.log(
+    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
+  );
+  console.log("\n-------------\n");
+};
+
+Intern.prototype.printInfo = function () {
+  console.log(
+    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nSchool: ${this.school}`
+  );
+  console.log("\n-------------\n");
+};
+
+const jeff = new Manager("Jeff", "Manager", 1, "email@email.com", 1);
+const alec = new EmployeeOne("Alec", "Engineer", 2, "alec@email.com", "alec1");
+const lindsey = new EmployeeTwo(
+  "Lindsey",
+  "Engineer",
+  3,
+  "lindsey@email.com",
+  "lindseyw"
+);
+const james = new EmployeeThree(
+  "James",
+  "Engineer",
+  4,
+  "james@email.com",
+  "jamesy2"
+);
+const molly = new Intern(
+  "Molly",
+  "Intern",
+  5,
+  "molly@email.com",
+  "Tech University"
+);
+
+jeff.printInfo();
+alec.printInfo();
+lindsey.printInfo();
+james.printInfo();
+molly.printInfo();
