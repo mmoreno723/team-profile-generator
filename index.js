2f0d34a3da9c6ea2781c87ae8b6e3bbfa4217f70
michael moreno <michaelmoreno@MacBook-Pro-7.local>
2022-06-06 23:10:47 -0400

    lib classes updated so they extend the employee class

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const inquirer = require("inquirer");
+const Employee = require("./lib/employee");
+
+const value = new Employee();
+
 function Manager(name, occupation, ID, email, officeNum) {
   this.name = name;
   this.occupation = occupation;
@@ -38,67 +43,96 @@ function Intern(name, occupation, ID, email, school) {
   this.school = school;
 }
 
-Manager.prototype.printInfo = function () {
-  console.log(
-    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nOffice Number: ${this.officeNum}`
-  );
-  console.log("\n-------------\n");
-};
+// Manager.prototype.printInfo = function () {
+//   console.log(
+//     `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nOffice Number: ${this.officeNum}`
+//   );
+//console.log("\n-------------\n");
+//};
 
-EmployeeOne.prototype.printInfo = function () {
-  console.log(
-    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
-  );
-  console.log("\n-------------\n");
-};
+// EmployeeOne.prototype.printInfo = function () {
+//     console.log(
+//     `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
+//   );
+//console.log("\n-------------\n");
+//};
 
-EmployeeTwo.prototype.printInfo = function () {
-  console.log(
-    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
-  );
-  console.log("\n-------------\n");
-};
+// EmployeeTwo.prototype.printInfo = function () {
+//   console.log(
+//     `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
+//   );
+//console.log("\n-------------\n");
+//};
 
-EmployeeThree.prototype.printInfo = function () {
-  console.log(
-    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
-  );
-  console.log("\n-------------\n");
-};
+// EmployeeThree.prototype.printInfo = function () {
+//   console.log(
+//     `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nGitHub: ${this.github}`
+//   );
+//   console.log("\n-------------\n");
+//};
 
-Intern.prototype.printInfo = function () {
-  console.log(
-    `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nSchool: ${this.school}`
-  );
-  console.log("\n-------------\n");
-};
+// Intern.prototype.printInfo = function () {
+//   console.log(
+//     `Name: ${this.name}\nOccupation: ${this.occupation}\nID: ${this.ID}\nEmail: ${this.email}\nSchool: ${this.school}`
+//   );
+//   console.log("\n-------------\n");
+//};
 
-const jeff = new Manager("Jeff", "Manager", 1, "email@email.com", 1);
-const alec = new EmployeeOne("Alec", "Engineer", 2, "alec@email.com", "alec1");
-const lindsey = new EmployeeTwo(
-  "Lindsey",
-  "Engineer",
-  3,
-  "lindsey@email.com",
-  "lindseyw"
-);
-const james = new EmployeeThree(
-  "James",
-  "Engineer",
-  4,
-  "james@email.com",
-  "jamesy2"
-);
-const molly = new Intern(
-  "Molly",
-  "Intern",
-  5,
-  "molly@email.com",
-  "Tech University"
-);
+// const jeff = new Manager("Jeff", "Manager", 1, "email@email.com", 1);
+// const alec = new EmployeeOne("Alec", "Engineer", 2, "alec@email.com", "alec1");
+// const lindsey = new EmployeeTwo(
+//   "Lindsey",
+//   "Engineer",
+//   3,
+//   "lindsey@email.com",
+//   "lindseyw"
+// );
+// const james = new EmployeeThree(
+//   "James",
+//   "Engineer",
+//   4,
+//   "james@email.com",
+//   "jamesy2"
+// );
+// const molly = new Intern(
+//   "Molly",
+//   "Intern",
+//   5,
+//   "molly@email.com",
+//   "Tech University"
+// );
 
-jeff.printInfo();
-alec.printInfo();
-lindsey.printInfo();
-james.printInfo();
-molly.printInfo();
+// jeff.printInfo();
+// alec.printInfo();
+// lindsey.printInfo();
+// james.printInfo();
+// molly.printInfo();
+
+inquirer.prompt([
+  {
+    type: "input",
+    message: "Please enter the name of the manager:",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "What is your manager's ID number?",
+    name: "managerID",
+  },
+  {
+    type: "input",
+    message: "WHat is your manager's email address?",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "What is your manager's office number?",
+    name: "managerOfficeNum",
+  },
+  {
+    type: "list",
+    message: "Would you like to add an engineer or intern?",
+    name: "Eng",
+    choices: ["Engineer", "Intern"],
+  },
+]);
